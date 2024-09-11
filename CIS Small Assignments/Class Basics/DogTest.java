07e693d64b4ecff340e1eea4d7a7eeb3f78c3189
Thom Mondeaux <Thommond554@gmail.com>
2024-09-11 15:38:46 -0400

    Added client class code

--- CIS Small Assignments/Class Basics/DogTest.java
+++ CIS Small Assignments/Class Basics/DogTest.java
@@ -1,5 +1,17 @@
 
+// Client class of DogTest 
+public class DogTest {  
+  public static void main(String[] args) {
+    Dog dog1 = new Dog("Fido", 5, 3);
+    Dog dog2 = new Dog("Rex");
+    Dog dog3 = new Dog();
 
-public class DogTest {
-    
+    System.out.println(dog1.getName());
+    System.out.println(dog1.getWeight());
+    System.out.println(dog1.getYear());
+    System.out.println(dog1.obtainSize());
+    System.out.println(dog2.getName());
+    System.out.println(dog3.getName());
+  }
 }
+
